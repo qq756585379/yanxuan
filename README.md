47b7eb2c6aa5c894c9aec904bf8325c9f0bf3c66
杨俊 <756585379@qq.com>
2017-07-21 18:14:32 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# yanxuan
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+
+cnpm i axios -S
+cnpm i mint-ui -S
+cnpm i vuex -S
+cnpm i iview -S
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/main.js
@@ -0,0 +1,31 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import MintUI from 'mint-ui';
+import Vuex from 'vuex';
+import iView from 'iview';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+
+import 'mint-ui/lib/style.css';
+import 'iview/dist/styles/iview.css';
+
+import store from './vuex/store';
+
+Vue.config.productionTip = false;
+
+Vue.use(MintUI);
+Vue.use(Vuex);
+Vue.use(iView);
+Vue.use(VueAwesomeSwiper);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  Vuex,
+  store,
+  template: '<App/>',
+  components: { App }
+});
